72efe608d79076699e8924ed11a1f62acf2cbdd9
Said Shatila <said.shatila@totersapp.com>
2023-10-26 15:03:21 +0300

    latest additions in compose state and some minor adjustments

---
+++ app/src/main/java/com/example/composetestlab/composestates/ComposeStatesDemo.kt
@@ -0,0 +1,31 @@
+package com.example.composetestlab.composestates
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ComposeStatesDemo() {
+    WaterCounter(modifier = Modifier.padding(16.dp))
+}
+
+@Composable
+private fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        val count: MutableState<Int> = remember { mutableStateOf(0) }
+        Text(
+            text = "You've had ${count.value} glasses.",
+            modifier = modifier.padding(16.dp)
+        )
+        Button(onClick = { count.value++ }, modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
+}
